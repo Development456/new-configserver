ea3be261928956831af07593627bfa8b2d3f06de
ineeladri <110054194+ineeladri@users.noreply.github.com>
2023-01-27 00:37:59 +0530

    Add files via upload

---
+++ src/test/java/com/miracle/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miracle.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
